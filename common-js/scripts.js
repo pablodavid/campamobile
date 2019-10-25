cc8c62c4638bab11eefc357106708bcf2a2ac678
Pablo Figueroa Vargas <42844957+pablodavid@users.noreply.github.com>
2019-10-25 18:59:06 -0300

    Add files via upload

---
+++ common-js/scripts.js
@@ -0,0 +1,421 @@
+
+	
+(function ($) {
+
+    "use strict";
+
+	// JQUERY LIGHT BOX
+	
+	if ( $.isFunction($.fn.fluidbox) ) {
+		$('a').fluidbox();
+	}
+	// Detect logo dimensions and add correct class
+	
+	var logoImage = $('.top-bar .logo:first-of-type');
+	
+	var theImage = new Image();
+	theImage.src = logoImage.attr("src");
+	
+	var logoWidth = theImage.width;
+	var logoHeight = theImage.height;
+	var logoRatio = logoWidth / logoHeight;
+	
+	if(logoRatio > 2.8){
+		$('.top-bar .logo').addClass('logo-wide');
+	}
+	
+	if(logoRatio < 2){
+		$('.top-bar .logo').addClass('logo-square');
+	}
+	
+	//ROUNDED TIMES COUNTDOWN
+	
+	if(isExists('#rounded-countdown')){
+		var remainingSec = $('.countdown').data('remaining-sec');
+		$('.countdown').ClassyCountdown({
+			theme: "flat-colors-very-wide",
+			end: $.now() + remainingSec
+		});
+	}
+	
+	//NORMAL TIMES COUNTDOWN
+	
+	if(isExists('#normal-countdown')){
+		var date = $('#normal-countdown').data('date');
+		$('#normal-countdown').countdown(date, function(event) {
+			var $this = $(this).html(event.strftime(''
+				+ '<div class="time-sec"><h3 class="main-time">%D</h3> <span>Días</span></div>'
+				+ '<div class="time-sec"><h3 class="main-time">%H</h3> <span>Horas</span></div>'
+				+ '<div class="time-sec"><h3 class="main-time">%M</h3> <span>Minutos</span></div>'
+				+ '<div class="time-sec"><h3 class="main-time">%S</h3> <span>Segundos</span></div>'));
+		});
+	}
+	
+	
+	$('a[href="#"]').on('click', function(event){
+		return;
+	});
+	
+	// COUNTDOWN TIME 
+	
+	countdownTime();
+	
+	
+	$('[data-nav-menu]').on('click', function(event){
+		
+		var $this = $(this),
+			visibleHeadArea = $this.data('nav-menu');
+		
+		$(visibleHeadArea).toggleClass('visible');
+		
+	});
+	
+	
+	var winWidth = $(window).width();
+	dropdownMenu(winWidth);
+	
+	$(window).on('resize', function(){
+		dropdownMenu(winWidth);
+		
+	});
+	
+	// Circular Progress Bar
+	
+	var isAnimated = false;
+	
+	// Contact form code
+
+    $('form.email-form').submit(function (e) {
+		// return false so form submits through jQuery rather than reloading page.
+		if(e.preventDefault) e.preventDefault(); 
+		else e.returnValue = false;
+		
+		var thisForm 		= $(this).closest('.email-form'),
+			error 			= 0,
+			originalError 	= thisForm.attr('original-error'),
+			loadingSpinner;
+			
+		if (typeof originalError !== typeof undefined && originalError !== false) {
+			thisForm.find('.form-error').text(originalError); 
+		}
+				
+
+		$(thisForm).find('.validate-required').each(function(){
+			if($(this).val() === ''){
+				$(this).addClass('field-error');
+				error = 1;
+			}else{
+				$(this).removeClass('field-error');
+			}
+		});
+		
+		$(thisForm).find('.validate-email').each(function(){
+			if(!(/(.+)@(.+){2,}\.(.+){2,}/.test($(this).val()))){
+				$(this).addClass('field-error');
+				error = 1;
+			}else{
+				$(this).removeClass('field-error');
+			}
+		});
+		
+
+        if (error === 1){
+            $(this).closest('.email-form').find('.form-error').fadeIn(200);
+        }else {
+			// Hide the error if one was shown
+			$(this).closest('.email-form').find('.form-error').fadeOut(200);
+			// Create a new loading spinner while hiding the submit button.
+			loadingSpinner = $('<div />').addClass('form-loading').insertAfter($(thisForm).find('input[type="submit"]'));
+			$(thisForm).find('input[type="submit"]').hide();
+            
+            jQuery.ajax({
+                type: "POST",
+                url: "mail/mail.php",
+                data: thisForm.serialize(),
+                success: function (response) {
+                	// Swiftmailer always sends back a number representing numner of emails sent.
+					// If this is numeric (not Swift Mailer error text) AND greater than 0 then show success message.
+					$(thisForm).find('.form-loading').remove();
+					$(thisForm).find('input[type="submit"]').show();
+					if($.isNumeric(response)){
+						if(parseInt(response) > 0){
+							thisForm.find('.form-success').fadeIn(1000);
+							thisForm.find('.form-error').fadeOut(1000);
+							setTimeout(function(){ thisForm.find('.form-success').fadeOut(500); }, 5000);
+						}
+					}
+					// If error text was returned, put the text in the .form-error div and show it.
+					else{
+						// Keep the current error text in a data attribute on the form
+						thisForm.find('.form-error').attr('original-error', thisForm.find('.form-error').text());
+						// Show the error with the returned error text.
+						thisForm.find('.form-error').text(response).fadeIn(1000);
+						thisForm.find('.form-success').fadeOut(1000);
+					}
+                },
+                error: function (errorObject, errorText, errorHTTP) {
+                	// Keep the current error text in a data attribute on the form
+					thisForm.find('.form-error').attr('original-error', thisForm.find('.form-error').text());
+					// Show the error with the returned error text.
+					thisForm.find('.form-error').text(errorHTTP).fadeIn(1000);
+					thisForm.find('.form-success').fadeOut(1000);
+                	$(thisForm).find('.form-loading').remove();
+					$(thisForm).find('input[type="submit"]').show();
+                }
+            });
+        }
+		return false;
+    });
+	
+	
+	
+	
+	
+	
+	
+	// Mailchimp/Campaign Monitor Mail List Form Scripts
+	$('form.mail-list-signup').on('submit', function(){
+		
+		var iFrame = $(this).closest('section, header').find('iframe.mail-list-form'),
+		thisForm 		= $(this).closest('.mail-list-signup'),
+		userEmail 		= $(this).find('.signup-email-field').val(),
+		userFullName 	= $(this).find('.signup-name-field').val(),
+		userFirstName 	= $(this).find('.signup-first-name-field').val(),
+		userLastName 	= $(this).find('.signup-last-name-field').val(),
+		error			= 0;
+		
+		$(thisForm).find('.validate-required').each(function(){
+			if($(this).val() === ''){
+				$(this).addClass('field-error');
+				error = 1;
+			}
+			else{
+				$(this).removeClass('field-error');
+			}
+		});
+		
+		$(thisForm).find('.validate-email').each(function(){
+			if(!(/(.+)@(.+){2,}\.(.+){2,}/.test($(this).val()))){
+				$(this).addClass('field-error');
+				error = 1;
+			}
+			else{
+				$(this).removeClass('field-error');
+			}
+		});
+		
+		if(error === 0){
+			iFrame.contents().find('#mce-EMAIL, #fieldEmail').val(userEmail);
+			iFrame.contents().find('#mce-LNAME, #fieldLastName').val(userLastName);
+			iFrame.contents().find('#mce-FNAME, #fieldFirstName').val(userFirstName);
+			iFrame.contents().find('#mce-FNAME, #fieldName').val(userFullName);		
+			iFrame.contents().find('form').attr('target', '_blank').submit();
+		}
+		return false;
+	});
+	
+	
+})(jQuery);
+
+
+
+function countdownTime(){
+	
+	if(isExists('#clock')){
+		$('#clock').countdown('2018/01/01', function(event){
+			var $this = $(this).html(event.strftime(''
+				+ '<div class="time-sec"><span class="title">%D</span> days </div>'
+				+ '<div class="time-sec"><span class="title">%H</span> hours </div>'
+				+ '<div class="time-sec"><span class="title">%M</span> minutes </div>'
+				+ '<div class="time-sec"><span class="title">%S</span> seconds </div>'));
+		});
+	}
+}
+function dropdownMenu(winWidth){
+	
+	if(winWidth > 767){
+		
+		$('.main-menu li.drop-down').on('mouseover', function(){
+			var $this = $(this),
+				menuAnchor = $this.children('a');
+				
+			menuAnchor.addClass('mouseover');
+			
+		}).on('mouseleave', function(){
+			var $this = $(this),
+				menuAnchor = $this.children('a');
+				
+			menuAnchor.removeClass('mouseover');
+		});
+		
+	}else{
+		
+		$('.main-menu li.drop-down > a').on('click', function(){
+			
+			if($(this).attr('href') == '#') return false;
+			if($(this).hasClass('mouseover')){ $(this).removeClass('mouseover'); }
+			else{ $(this).addClass('mouseover'); }
+			return false;
+		});
+	}
+	
+}
+
+function isExists(elem){
+	if ($(elem).length > 0) { 
+		return true;
+	}
+	return false;
+}
+
+function initMap() {
+
+	// Create a new StyledMapType object, passing it an array of styles,
+	// and the name to be displayed on the map type control.
+	var styledMapType = new google.maps.StyledMapType(
+	[
+		{
+			"featureType": "administrative",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": "-100"
+				}
+			]
+		},
+		{
+			"featureType": "administrative.province",
+			"elementType": "all",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "landscape",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": -100
+				},
+				{
+					"lightness": 65
+				},
+				{
+					"visibility": "on"
+				}
+			]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": -100
+				},
+				{
+					"lightness": "50"
+				},
+				{
+					"visibility": "simplified"
+				}
+			]
+		},
+		{
+			"featureType": "road",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": "-100"
+				}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "all",
+			"stylers": [
+				{
+					"visibility": "simplified"
+				}
+			]
+		},
+		{
+			"featureType": "road.arterial",
+			"elementType": "all",
+			"stylers": [
+				{
+					"lightness": "30"
+				}
+			]
+		},
+		{
+			"featureType": "road.local",
+			"elementType": "all",
+			"stylers": [
+				{
+					"lightness": "40"
+				}
+			]
+		},
+		{
+			"featureType": "transit",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": -100
+				},
+				{
+					"visibility": "simplified"
+				}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"hue": "#ffff00"
+				},
+				{
+					"lightness": -25
+				},
+				{
+					"saturation": -97
+				}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "labels",
+			"stylers": [
+				{
+					"lightness": -25
+				},
+				{
+					"saturation": -100
+				}
+			]
+		}
+	],
+		{name: 'Styled Map'});
+
+	// Create a map object, and include the MapTypeId to add
+	// to the map type control.
+
+	var uluru = {lat: 56.946285, lng: 24.105078};
+    var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 4,
+		center: uluru
+    });
+		
+	var image = 'images/google-marker.png';
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map,
+		icon: image
+	});
+	//Associate the styled map with the MapTypeId and set it to display.
+	map.mapTypes.set('styled_map', styledMapType);
+	map.setMapTypeId('styled_map');
+}
